2540c11eadd6b4d17e654316c599d25b8e9d9e67
despina <dkaravia@gmail.com>
2020-03-19 00:09:02 +0200

    Get infromation for and create student and trainer objects

--- src/privateschool/MainClass.java
+++ src/privateschool/MainClass.java
@@ -1,21 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package privateschool;
 
-/**
- *
- * @author deka
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MainClass {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        ArrayList<Student> listOfStudents = new ArrayList();
+        ArrayList<Trainer> listOfTrainers = new ArrayList();
+
+        listOfStudents.add(createStudent());
+        listOfTrainers.add(createTrainer());
+
+    }
+
+    public static Student createStudent() {
+
+        String firstName = getName("Student first name: ");
+        String lastName = getName("Student last name: ");
+        LocalDate dateOfBirth = getDateOfBirth();
+        double tuitionFees = getTuitionFees();
+        Student student = new Student(firstName, lastName, dateOfBirth, tuitionFees);
+        return student;
+    }
+
+    public static Trainer createTrainer() {
+        String firstName = getName("Trainer first name: ");
+        String lastName = getName("Trainer last name: ");
+        Trainer trainer = new Trainer(firstName, lastName);
+        return trainer;
+    }
+
+    public static String getName(String message) {
+        System.out.print(message);
+        return input.next();
+    }
+
+    private static LocalDate getDateOfBirth() {
+
+        System.out.print("Student date of birth(YYYY-MM-DD)?");
+        String date = input.next();
+        while (date.charAt(4) != '-' || date.charAt(7) != '-') {
+            System.out.print("Please provide the date in the format YYYY-MM-DD");
+            date = input.next();
+        }
+        String[] dateArray = date.split("-");
+        int year = Integer.parseInt(dateArray[0]);
+        int month = Integer.parseInt(dateArray[1]);
+        int day = Integer.parseInt(dateArray[2]);
+
+        LocalDate localDate = LocalDate.of(year, month, day);
+        return localDate;
+    }
+
+    private static double getTuitionFees() {
+        double tuitionFees;
+        do {
+            System.out.print("Tuition fees: ");
+            while (!input.hasNextDouble()) {
+                System.out.print("Tuition fees: ");
+                input.next();
+            }
+            tuitionFees = input.nextDouble();
+        } while (tuitionFees < 0);
+        return tuitionFees;
     }
     
 }
