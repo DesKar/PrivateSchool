2b44b7ca3c8ef7b3a5ef462318876ee7f963d795
despina <dkaravia@gmail.com>
2020-03-19 18:41:26 +0200

    Add subject as Trainer attribute

--- src/privateschool/MainClass.java
+++ src/privateschool/MainClass.java
@@ -13,10 +13,12 @@ public class MainClass {
         ArrayList<Student> listOfStudents = new ArrayList();
         ArrayList<Trainer> listOfTrainers = new ArrayList();
         ArrayList<Assignment> listOfAssignments = new ArrayList();
-
+        ArrayList<Course> listOfCourses = new ArrayList();
+        
         listOfStudents.add(createStudent());
         listOfTrainers.add(createTrainer());
         listOfAssignments.add(createAssignment());
+        listOfCourses.add(createCourse());
 
     }
 
@@ -41,10 +43,20 @@ public class MainClass {
         String title = getStringWithMessage("Assignment title: ");
         String description = getStringWithMessage("Assignment description: ");
         LocalDate subDateTime = getDate("Assignment submission date (YYYY-MM-DD): ");
-        int oranMark = getIntNumber("Oral Mark: ", "Please provide a number between 1-10");
-        int localMark = getIntNumber("Local mark: ", "Please provide a number between 1-10");
+        int oranMark = getIntNumber("Oral Mark: ", "Please provide a number between 1-10: ");
+        int localMark = getIntNumber("Local mark: ", "Please provide a number between 1-10: ");
         Assignment assignment = new Assignment(title, description, subDateTime, oranMark, localMark);
         return assignment;
+    }
+
+    public static Course createCourse() {
+        String title = getStringWithMessage("Course title: ");
+        String stream = getStringWithMessage("Course stream title: ");
+        String type = getStringWithMessage("Course type: ");
+        LocalDate startDate = getDate("Course starting date: ");
+        LocalDate endDate = getDate("Course starting date: ");
+        Course course = new Course(title, stream, type, startDate, endDate);
+        return course;
     }
 
     public static String getStringWithMessage(String message) {
@@ -64,6 +76,8 @@ public class MainClass {
         int year = Integer.parseInt(dateArray[0]);
         int month = Integer.parseInt(dateArray[1]);
         int day = Integer.parseInt(dateArray[2]);
+        
+        //TODO check validity of date
 
         LocalDate localDate = LocalDate.of(year, month, day);
         return localDate;
