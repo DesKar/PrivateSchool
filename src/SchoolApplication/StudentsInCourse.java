b3f98d87c94b6964b5335ea1b7b87aa9659df177
despina <dkaravia@gmail.com>
2020-03-22 01:36:21 +0200

    Create new Class StudentsInCourse

---
+++ src/SchoolApplication/StudentsInCourse.java
@@ -0,0 +1,16 @@
+
+package SchoolApplication;
+
+import java.util.ArrayList;
+
+public class StudentsInCourse {
+    Course course;
+    ArrayList<Student> listOfStudents;
+
+    public StudentsInCourse(Course course, ArrayList<Student> listOfStudents) {
+        this.course = course;
+        this.listOfStudents = listOfStudents;
+    }
+    
+    
+}
