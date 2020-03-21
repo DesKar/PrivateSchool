28b560c2b5a7c3f6d8d668e71e6b4e0fa3e37bba
despina <dkaravia@gmail.com>
2020-03-21 23:24:01 +0200

    Improve printing lists

--- src/privateschool/School.java
+++ src/privateschool/School.java
@@ -42,43 +42,47 @@ public class School {
     }
 
     public void printListOfStudents() {
+        System.out.println("--------------------------------Students--------------------------------\n");
         String header = String.format("|%-5s|%-15s|%-15s|%-15s|%-15s|", "ID", "First name", "Last name", "Date of Birth", "Tuition Fees(€)");
         System.out.println(header);
         for (Student student : listOfStudents) {
             String content = String.format("|%-5s|%-15s|%-15s|%-15s|%-15s|", listOfStudents.indexOf(student) + 1, student.getFirstName(), student.getLastName(), student.getDateOfBirth(), student.getTuitionFees());
             System.out.println(content);
         }
-        System.out.println("");
+        System.out.println("------------------------------------------------------------------------");
     }
 
     public void printListOfTrainers() {
+        System.out.println("----------------Trainers---------------\n");
         String header = String.format("|%-5s|%-15s|%-15s|", "ID", "First Name", "Last Name");
         System.out.println(header);
         for (Trainer trainer : listOfTrainers) {
-            String content = String.format("|%-5s|%-15s|%-15s|", listOfTrainers.indexOf(trainer) + 1,  trainer.getFirstName(), trainer.getLastName());
+            String content = String.format("|%-5s|%-15s|%-15s|", listOfTrainers.indexOf(trainer) + 1, trainer.getFirstName(), trainer.getLastName());
             System.out.println(content);
         }
-        System.out.println("");
+        System.out.println("---------------------------------------");
     }
 
     public void printListOfAssignments() {
+        System.out.println("-------------------------------------------------------Assignments-------------------------------------------------------\n");
         String header = String.format("|%-5s|%-25s|%-40s|%-18s|%-13s|%-13s|", "ID", "Title", "Description", "Submission Date", "Oral Mark", "Local Mark");
         System.out.println(header);
         for (Assignment assignment : listOfAssignments) {
             String content = String.format("|%-5s|%-25s|%-40s|%-18s|%-13d|%-13d|", listOfAssignments.indexOf(assignment) + 1, assignment.getTitle(), assignment.getDescription(), assignment.getSubDateTime(), assignment.getOralMark(), assignment.getLocalMark());
             System.out.println(content);
         }
-        System.out.println("");
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+
     }
 
     public void printListOfCourses() {
+        System.out.println("--------------------------------------------------Courses-------------------------------------------------");
         String header = String.format("|%-5s|%-25s|%-30s|%-13s|%-13s|%-13s|", "ID", "Title", "Stream", "Type", "Start Date", "End Date");
         System.out.println(header);
         for (Course course : listOfCourses) {
             String content = String.format("|%-5s|%-25s|%-30s|%-13s|%-13s|%-13s|", listOfCourses.indexOf(course) + 1, course.getTitle(), course.getStream(), course.getType(), course.getStartDate(), course.getEndDate());
             System.out.println(content);
         }
-        System.out.println("");
-
+        System.out.println("----------------------------------------------------------------------------------------------------------");
     }
 }
