ada8ceff162d95b97da1466a42d3f91efc2de9f1
despina <dkaravia@gmail.com>
2020-04-20 01:35:29 +0300

    Add basic validations

--- front_end/courses.js
+++ front_end/courses.js
@@ -3,20 +3,21 @@ document.querySelector("form").addEventListener("submit", function (e) {
     document.getElementById("alert").innerHTML = "";
     const form = this;
 
-    const start_date = form.start_date.value;
-    console.log(start_date);
-
-    const end_date = form.end_date.value;
-    
-    if(start_date>end_date){
-        addAlert("Starting date should be before ending date.")
-    };;
-
+    validate_start_end_dates(form);
 })
 
-function addAlert(message){
-    document.getElementById("alert").innerHTML = 
-    `<div class="alert alert-warning alert-dismissible fade show" role="alert">
+function validate_start_end_dates(form) {
+    const start_date = form.start_date.value;
+    const end_date = form.end_date.value;
+
+    if (start_date > end_date) {
+        addAlert("Starting date should be before ending date.")
+    };
+}
+
+function addAlert(message) {
+    document.getElementById("alert").innerHTML =
+        `<div class="alert alert-warning alert-dismissible fade show" role="alert">
         ${message}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
