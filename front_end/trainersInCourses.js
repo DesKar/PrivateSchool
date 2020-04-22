69498ce648b3611e0de622077b7427235980f9c0
despina <dkaravia@gmail.com>
2020-04-22 11:56:46 +0300

    Add demo data in trainers in courses

---
+++ front_end/trainersInCourses.js
@@ -0,0 +1,24 @@
+function addDemoData() {
+    const demoData = [
+        {
+            course: "Mathematics",
+            students: [
+                "Helen Moss",
+            ]
+        },
+        {
+            course: "Algorithms",
+            students: [
+                "Marianne Uriol",
+            ]
+        },
+        {
+            course: "Cooking",
+            students: [
+                "John Oliver",
+                "Martin Nusli",
+            ]
+        }
+
+    ]
+}
