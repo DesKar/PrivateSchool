d5d8b2e114f3d4b93943aad882f5ca9946860f89
despina <dkaravia@gmail.com>
2020-04-22 11:56:15 +0300

    Add demo data in assignments in courses

---
+++ front_end/assignmentsInCourses.js
@@ -0,0 +1,25 @@
+function addDemoData() {
+    const demoData = [
+        {
+            course: "Mathematics",
+            students: [
+                "Linear Algebra 1",
+                "Linear Algebra 2",
+            ]
+        },
+        {
+            course: "Algorithms",
+            students: [
+                "Binary Search algorithm",
+                "Buchberger's algorithm",
+            ]
+        },
+        {
+            course: "Art History",
+            students: [
+                "Essay on El Greco",
+                "Marc Chagall",
+            ]
+        }
+    ]
+}
